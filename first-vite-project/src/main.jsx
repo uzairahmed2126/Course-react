e1763bbf6f1c3d3dbe835cfd56ed5e0cdfed187f
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-12-24 19:01:12 +0530

    practice on react

--- first-vite-project/src/main.jsx
+++ first-vite-project/src/main.jsx
@@ -1,9 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const anotherElement = (
+  <a href="https://google.com" target="_blank">
+    Visit google
+  </a>
+);
+createRoot(document.getElementById("root")).render(
+  // <StrictMode>
+  //   <App />
+  anotherElement
+  // </StrictMode>
+);
