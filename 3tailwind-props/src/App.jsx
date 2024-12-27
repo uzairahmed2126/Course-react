d13b950bbca5f137f1f4e67df00497799ee85b34
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-12-27 08:55:56 +0530

    tailwind

--- 3tailwind-props/src/App.jsx
+++ 3tailwind-props/src/App.jsx
@@ -5,17 +5,19 @@ import Card from "./components/Card";
 function App() {
   return (
     <>
-      <h1 className="bg-yellow-900 text-green-600 text-20">Hello</h1>
-      <Card
-        username={"uzair"}
-        text={"Hello i'm a react dev..."}
-        btn={"check out!"}
-      />
-      <Card
-        username={"Ahmed"}
-        text={"Hello i'm a full-stack dev..."}
-        btn={"click it!"}
-      />
+      <>
+        <h1 className="bg-yellow-900 text-green-600 text-20">Hello</h1>
+        <Card
+          username={"uzair"}
+          text={"Hello i'm a react dev..."}
+          btn={"check out!"}
+        />
+        <Card
+          username={"Ahmed"}
+          text={"Hello i'm a full-stack dev..."}
+          btn={"click it!"}
+        />
+      </>
     </>
   );
 }
